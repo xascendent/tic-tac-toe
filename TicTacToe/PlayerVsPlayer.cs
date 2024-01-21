e48ec8e1dcba4f7f70e11061e81a5fdf09dc61d6
john <john.brumett@hsc.utah.edu>
2024-01-21 12:36:55 -0700

    init commit

---
+++ TicTacToe/PlayerVsPlayer.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class PlayerVsPlayer
+    {
+
+        //Console.WriteLine("_1_|_2_|_3_");
+        //Console.WriteLine("_4_|_5_|_6_");
+        //Console.WriteLine("_7_|_8_|_9_");
+
+        // if MoveResult is 0 then move is invalid
+        // if MoveResult is 1 then move is valid
+        // if MoveResult is 2 then game is over X wins
+        // if MoveResult is 3 then game is over O wins
+        // if MoveResult is 4 then game is over draw
+
+        public bool ActiveGame = true;
+
+        public int MoveResult(string[,] gameBoard, string playerToken, int rowNumber, int columnNumber)
+        {            
+            // test if the location is already taken
+            if (gameBoard[rowNumber, columnNumber] != null)
+            {                
+                return 0; // invalid move
+            }
+            else
+            {
+                gameBoard[rowNumber, columnNumber] = playerToken;                
+            }
+            // check for win X
+            if (CheckForWin(gameBoard, "X") == true) {
+                ActiveGame = false;
+                return 2; };
+            // check for win O
+            if (CheckForWin(gameBoard, "O") == true) {
+                ActiveGame = false;
+                return 3; } ;
+            // check for draw
+            if (CheckForDraw(gameBoard) == true) {
+                ActiveGame = false;
+                return 4; } ;
+            return 1;
+        }
+        public bool CheckForDraw(string[,] gameBoard)
+        {
+            // check if all locations are taken
+            for (int row = 0; row < 3; row++) { 
+                for(int col = 0; col < 3; col++)
+                {
+                    if (gameBoard[row, col] == null)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public bool CheckForWin(string[,] gameBoard, string playerToken)
+        {            
+            // see if any rows are winners for X
+            if (CheckRowForWin(gameBoard, playerToken, 0) == true) { return true; }
+            if (CheckRowForWin(gameBoard, playerToken, 1) == true) { return true; }
+            if (CheckRowForWin(gameBoard, playerToken, 2) == true) { return true; }
+
+            // see if any cols are winners for X
+            if (CheckColForWin(gameBoard, playerToken, 0) == true) { return true; }
+            if (CheckColForWin(gameBoard, playerToken, 1) == true) { return true; }
+            if (CheckColForWin(gameBoard, playerToken, 2) == true) { return true; }
+
+            // see if any crosses are winners for X
+            if (CheckCrossForWin(gameBoard, playerToken, 1) == true) { return true; }
+            if (CheckCrossForWin(gameBoard, playerToken, 2) == true) { return true; }
+
+            return false;
+        }
+
+        public bool CheckRowForWin(string[,] gameBoard, string playerToken, int rowNumber)
+        {   
+            if (gameBoard[rowNumber, 0] == playerToken && gameBoard[rowNumber, 1] == playerToken && gameBoard[rowNumber, 2] == playerToken)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool CheckColForWin(string[,] gameBoard, string playerToken, int colNumber)
+        {
+            if (gameBoard[0, colNumber] == playerToken && gameBoard[1, colNumber] == playerToken && gameBoard[0, colNumber] == playerToken)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool CheckCrossForWin(string[,] gameBoard, string playerToken, int crossNumber)
+        {            
+            if(crossNumber == 1)
+            {
+                if (gameBoard[0, 0] == playerToken && gameBoard[1, 1] == playerToken && gameBoard[2, 2] == playerToken)
+                {
+                    return true;
+                }
+            }
+            if (crossNumber == 2)
+            {
+                if (gameBoard[0, 2] == playerToken && gameBoard[1, 1] == playerToken && gameBoard[2, 0] == playerToken)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ TicTacToe/Program.cs
@@ -0,0 +1,62 @@
+﻿using TicTacToe;
+
+Console.WriteLine("Hello, Welcome to Y3 TICTACTOE");
+Console.WriteLine("Pick your game type (1- pvp, 2- pve):");
+int gameType = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Pick your game piece (1- X, 2- O):");
+var gamePiece = Console.ReadLine();
+
+string[,] gameBoard = new string[3, 3];
+PlayerVsPlayer playerVsPlayer = new PlayerVsPlayer();
+
+Console.WriteLine("_0,0_|_0,1_|_0,2_");
+Console.WriteLine("_1,0_|_1,1_|_1,2_");
+Console.WriteLine("_2,0_|_2,1_|_2,2_");
+
+// game loop 
+string currentPlayer = "X";
+int row, col, moveResult;
+
+while (playerVsPlayer.ActiveGame)
+{
+    Console.WriteLine($"Pick your row player {currentPlayer} (0-2) :");
+    row = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Pick your col player {currentPlayer} (0-2) :");
+    col = Convert.ToInt32(Console.ReadLine());
+    moveResult = playerVsPlayer.MoveResult(gameBoard, currentPlayer, row, col);
+
+    if (moveResult == 0)
+    {
+        Console.WriteLine($"Move is invalid splace already taken");
+    }
+    if(moveResult == 1)
+    {
+        gameBoard[row, col] = currentPlayer;
+        if (currentPlayer == "X")
+        {
+            currentPlayer = "O";
+        }
+        else
+        {
+            currentPlayer = "X";
+        }
+    }
+    if (moveResult == 2)
+    {
+        Console.WriteLine($"X wins!");
+    }
+    if (moveResult == 3)
+    {
+        Console.WriteLine($"O wins!");
+    }
+    if (moveResult == 4)
+    {
+        Console.WriteLine($"Cat Game!");
+    }
+}
+
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Game Over thank you for playing!");
